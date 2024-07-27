d7cb1e6b86a7d3899768116db4308b99495c1420
Grzegorz Wojtanowicz <wojtanowiczgrzegorz@gmail.com>
2024-07-27 07:13:44 +0200

    print format seems to be ok - not styled

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,11 @@
 <script>
 	import Counter from './Counter.svelte';
 	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import Logo from './Logo.svelte';
+	import { mockData } from './mock-data';
+
+	let items = mockData;
+	let customText = 'klasa 1a';
 </script>
 
 <svelte:head>
@@ -10,50 +14,65 @@
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+	<div class="wrapper">
+		{#each items as { email, id, name }}
+			<div class="item-wrapper">
+				<div class="left">
+					<div class="picture"><Logo /></div>
+				</div>
+				<div class="right">
+					<div class="id">
+						<span class="small">numer zamówienia</span>
+						{id}
+					</div>
+					<div class="name">{name.toLowerCase()}</div>
+					<div class="custom">{customText}</div>
+				</div>
+			</div>
+		{/each}
+	</div>
 </section>
 
 <style>
-	section {
+	@page {
+		size: A4 portrait;
+		margin: 1mm 0mm;
+	}
+	.wrapper {
+		display: grid;
+		grid-template-columns: repeat(3, 1fr);
+	}
+	.item-wrapper {
+		aspect-ratio: 700/424; /* 70.0mm x 42.4mm https://neopak.pl/cms/szablony-etykiet */
+		padding: 5%;
+		border: 1px solid #ccc;
 		display: flex;
-		flex-direction: column;
+		gap: 5%;
+	}
+	.left {
+		width: 40%;
+		display: flex;
 		justify-content: center;
-		align-items: center;
-		flex: 0.6;
 	}
-
-	h1 {
-		width: 100%;
+	.right {
+		/* flex-grow: 1; */
 	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
+	.picture {
+		width: 150px;
 	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+	.small {
+		display: flex;
+		font-size: 9px;
+	}
+	.id {
+		flex-wrap: wrap;
+		font-size: 32px;
+	}
+	.name {
+		text-transform: capitalize;
+		/* width: 20px; */
+	}
+	.custom {
+		/* width: 20px; */
 	}
 </style>
